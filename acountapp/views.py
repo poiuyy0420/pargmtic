572914960bfc337a3d951213aa5b025978b20277
poiuyy0420 <poiuyy0420@gmail.com>
2021-03-12 17:48:11 +0900

    list update

--- acountapp/views.py
+++ acountapp/views.py
@@ -2,7 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from acountapp.models import HelloWorld
 
 
 def hello_world(request):
-    return render(request, 'acountapp/hello_world.html')
+    if request.method == 'POST':
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'acountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+    else:
+        return render(request, 'acountapp/hello_world.html', context={'text': 'GET METHOD!!!!'})
--- pargmtic/urls.py
+++ pargmtic/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('acountapp.urls'))
+    path('acount/', include('acountapp.urls'))
 ]
