82378317472d82380533c05a3ece5bad77e4acf9
poiuyy0420 <poiuyy0420@gmail.com>
2021-03-17 17:33:28 +0900

    subscribeapp

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,8 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -24,14 +26,19 @@ class ProjectCreateView(CreateView):
 
 class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
-    context_object_name = 'target-project'
+    context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
 
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
+        project = self.object
+        user = self.request.user
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+
         object_list = Article.objects.filter(project=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, subscription=subscription, **kwargs)
 
 
 class ProjectListView(ListView):
